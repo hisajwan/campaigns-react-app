290a4ae0fb7775d5df65e59bf55155338e5121f7
Himanshu Sajwan <Himanshu.Sajwan@XEBIAINDIA.LOCAL>
2019-11-04 00:13:13 +0530

    Updates

---
+++ precache-manifest.c0f2215a261c8d150c1027f3884d7657.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d382f48c4ae1b509bd7bcf08baa0db40",
+    "url": "/campaigns-react-app/index.html"
+  },
+  {
+    "revision": "d2c0aa1110df1f80c952",
+    "url": "/campaigns-react-app/static/css/2.cedc9c11.chunk.css"
+  },
+  {
+    "revision": "26238e3939eec3073936",
+    "url": "/campaigns-react-app/static/css/main.8a9ed375.chunk.css"
+  },
+  {
+    "revision": "d2c0aa1110df1f80c952",
+    "url": "/campaigns-react-app/static/js/2.73bf0873.chunk.js"
+  },
+  {
+    "revision": "26238e3939eec3073936",
+    "url": "/campaigns-react-app/static/js/main.726d147a.chunk.js"
+  },
+  {
+    "revision": "007cd9ce76c74c522efe",
+    "url": "/campaigns-react-app/static/js/runtime~main.fe6c0071.js"
+  },
+  {
+    "revision": "965fccbf2fa758c7e2e5fd0e96f016bb",
+    "url": "/campaigns-react-app/static/media/bluestacks_logo_cut.965fccbf.png"
+  },
+  {
+    "revision": "7173cfe7840a5f2d3853b4220f6cf851",
+    "url": "/campaigns-react-app/static/media/garena.7173cfe7.png"
+  },
+  {
+    "revision": "977ecf7be74437c437106e00ad13f379",
+    "url": "/campaigns-react-app/static/media/mancala_mix.977ecf7b.png"
+  },
+  {
+    "revision": "3ecfc5bd32f7be1a974631a1e152d0b3",
+    "url": "/campaigns-react-app/static/media/mole_slayer.3ecfc5bd.png"
+  },
+  {
+    "revision": "90640a8778af0217304f5effb1aa8413",
+    "url": "/campaigns-react-app/static/media/mortalcombat.90640a87.png"
+  },
+  {
+    "revision": "c161ba4abb12e86093bc396fd2337b9d",
+    "url": "/campaigns-react-app/static/media/needforspeed.c161ba4a.png"
+  },
+  {
+    "revision": "e51f42c349763884e809677c9634b288",
+    "url": "/campaigns-react-app/static/media/pubg.e51f42c3.png"
+  },
+  {
+    "revision": "8a4d68dbcc8a535baeeef974e67aaed2",
+    "url": "/campaigns-react-app/static/media/super_jewels_quest.8a4d68db.png"
+  }
+]);
